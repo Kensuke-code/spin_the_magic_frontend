f73e569959109355cdd1629c0a4f4bc9d300e927
k-okamoto <k-okamoto@wwwave.jp>
2023-10-31 13:32:08 +0900

    axiosの設定

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>hogehoge</div>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div>
+      {{ msgs }}
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage'
-})
+<script>
+export default {
+  data () {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.$get('/api/v1/spin_gacha').then(console.log('fetched')).catch(e => console.log(e))
+    // .then(res => this.msgs.push(res))
+    }
+  }
+}
 </script>
